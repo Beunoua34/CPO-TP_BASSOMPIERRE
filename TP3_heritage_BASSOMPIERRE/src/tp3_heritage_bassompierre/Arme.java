10e908e60e410a52d55888b71e5aad92601f6bb0
BENOIT  BASSOMPIERRE <benoit.bassompierre@epfedu.fr>
2022-11-08 15:29:00 +0100

    creation du projet et de la classe arme

---
+++ TP3_heritage_BASSOMPIERRE/src/tp3_heritage_bassompierre/Arme.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heritage_bassompierre;
+
+/**
+ *
+ * @author benba
+ */
+public class Arme {
+    String nom;
+    int niv_attaque;
+    public Arme(String blaze,int niv ){
+        nom= blaze;
+        niv_attaque=niv;
+    }
+}
---
+++ TP3_heritage_BASSOMPIERRE/src/tp3_heritage_bassompierre/TP3_heritage_BASSOMPIERRE.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_heritage_bassompierre;
+
+/**
+ *
+ * @author benba
+ */
+public class TP3_heritage_BASSOMPIERRE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Arme kalash=new Arme("criminel",16);
+        System.out.println(kalash);
+    }
+    
+}
